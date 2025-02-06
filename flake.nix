d21fffa55fbe9b7ddc7c6cb441d2e9b6d27b78d8
MathieuDR <mathieu@deraedt.dev>
2025-02-06 17:25:31 +0100

    🧑‍💻 flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NPM Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg:
+          builtins.elem (nixpkgs.lib.getName pkg) [
+          ];
+      };
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            npm i
+            npx quartz build --concurrency 6 --serve
+          '';
+        };
+      };
+    });
+}
