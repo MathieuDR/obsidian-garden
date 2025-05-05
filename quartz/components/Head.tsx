4bed9f768cc7740ab16e0b4fb5616134c9103b7e
MathieuDR <mathieu@deraedt.dev>
2025-05-05 23:07:51 +0200

    plausible outbound links

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -201,7 +201,7 @@ export default (() => {
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
           .map((res) => JSResourceToScriptElement(res, true))}
-        <script defer data-domain="mathieu.deraedt.dev" src="https://stats.deraedt.dev/js/script.js"></script>
+        <script defer data-domain="mathieu.deraedt.dev" src="https://stats.deraedt.dev/js/script.outbound-links.js"></script>
       </head>
     )
   }
