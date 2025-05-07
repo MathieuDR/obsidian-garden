9cbdb9d3474c60f0a98799dbaa2f35d00bb4debf
MathieuDR <mathieu@deraedt.dev>
2025-05-07 13:56:10 +0200

    📈 Script tag change

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: " | Growing ideas",
     enableSPA: true,
     enablePopovers: true,
-    analytics: { provider: 'plausible', host: 'https://insights.deraedt.dev', script: 'outbound-links' },
+    analytics: { provider: 'plausible', host: 'https://insights.deraedt.dev', script: 'outbound-links.pageview-props.tagged-events' },
     locale: "en-GB",
     baseUrl: "mathieu.deraedt.dev",
     ignorePatterns: [
