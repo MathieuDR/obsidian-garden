8159a6161df40c5c3db2df4c8a0ae47b316d5911
MathieuDR <mathieu@deraedt.dev>
2025-05-10 14:44:00 +0200

    remove aria

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -117,7 +117,7 @@ export default ((userOpts?: Partial<Options>) => {
             <polyline points="6 9 12 15 18 9"></polyline>
           </svg>
         </button>
-        <div id="explorer-content" class="explorer-content" aria-expanded={false}>
+        <div id="explorer-content" class="explorer-content">
           <OverflowList class="explorer-ul" />
         </div>
         <template id="template-file">
