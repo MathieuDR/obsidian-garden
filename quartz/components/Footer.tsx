59f4aaa0c3e8431dea2ac2fe25f2d983f1d90201
MathieuDR <mathieu@deraedt.dev>
2025-07-19 16:34:47 +0200

    remove email stuff

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -20,47 +20,6 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <div class="kit">
-          <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
-          <form
-            action="https://app.kit.com/forms/8145391/subscriptions"
-            class="seva-form formkit-form"
-            method="post"
-            data-sv-form="8145391"
-            data-uid="12a2816a55"
-            data-format="inline"
-            data-version="5"
-            data-options='{"settings":{"after_subscribe":{"action":"redirect","success_message":"Success! Now check your email to confirm your subscription.\nOr visi","redirect_url":"https://mathieu.deraedt.dev/output/pages/1749074946-confirm"},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":false,"url":"https://kit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
-            min-width="400 500 600 700 800"
-          >
-            <div data-style="clean">
-              <ul class="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
-              <div data-element="fields" data-stacked="false" class="seva-fields formkit-fields">
-                <div class="formkit-field">
-                  <input class="formkit-input" aria-label="Name" name="fields[first_name]" placeholder="Name" type="text" />
-                </div>
-                <div class="formkit-field">
-                  <input
-                    class="formkit-input"
-                    name="email_address"
-                    aria-label="Email Address"
-                    placeholder="Email Address"
-                    required
-                    type="email"
-                  />
-                </div>
-                <button data-element="submit" class="formkit-submit formkit-submit">
-                  <div class="formkit-spinner">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                  <span class="">Subscribe</span>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a class="external" href="https://quartz.jzhao.xyz/">
