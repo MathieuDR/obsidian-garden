24a373a9d8a0ef2fa5e2c5b2b9d5794d7c6e3b15
MathieuDR <mathieu@deraedt.dev>
2026-02-23 03:42:13 +0100

    auxclick

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -213,19 +213,19 @@ function addGlobalPageResources(ctx: BuildCtx, componentResources: ComponentReso
       );
 
       goatcounterScript.onload = () => {
-        function bindExternalLinks() {
-          document.querySelectorAll('a.external').forEach((link) => {
-            if (link._gcBound) return
-            link._gcBound = true
-            link.addEventListener('click', function() {
-              window.goatcounter.count({
-                path: 'external-' + link.href,
-                title: link.innerText || link.href,
-                event: true,
-              })
+        document.querySelectorAll('a.external').forEach((link) => {
+          if (link._gcBound) return
+          link._gcBound = true
+          const handler = function() {
+            window.goatcounter.count({
+              path: 'external-' + link.href,
+              title: link.innerText || link.href,
+              event: true,
             })
-          })
-        }
+          }
+          link.addEventListener('click', handler)
+          link.addEventListener('auxclick', handler) // middle-click
+        })
 
         const initialPath = window.goatcounter.path(location.pathname);
         goatcounter.count({ path: initialPath });
